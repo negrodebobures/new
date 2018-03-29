1d1ec26b72adea43435da46198e07b371459df46
chequeaditos <34919735+negrodebobures@users.noreply.github.com>
2018-03-28 21:50:39 -0400

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,9 @@
+window.sr = ScrollReveal({
+	opacity: 0
+});
+
+sr.reveal('.title', { duration: 3000, rotate: { x: 0, y: 0, z: 0 } });
+sr.reveal('.subtitle', { duration: 4000 });
+sr.reveal('#nav', { duration: 4000 });
+sr.reveal('#content', { duration: 3000 });
+sr.reveal('#footer', { duration: 6000 });
